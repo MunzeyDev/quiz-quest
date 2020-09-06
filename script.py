42e798ca6745db27c44e8930cf816394d11696ab
MunzeyDev <67990033+MunzeyDev@users.noreply.github.com>
2020-09-06 16:55:05 -0400

    Update script.py

--- script.py
+++ script.py
@@ -14,12 +14,12 @@ for i in range (3):
 print("                                                                    by: MunzeyDev")
 
 print("""1. The apple quiz
-2. (COMING SOON)
-""")
+2. The english quiz
+3. (COMING SOON)""")
 
 print("")
 choice = input("Choose a level: ")
-
+print("")
 if choice == "1":
     print("""Question A. Who is the founder of the company Apple?
 1. Toby Maguire
@@ -35,7 +35,7 @@ if choice == "1":
         var1 = input("Question A. What is your Answer?: ")
         if var1 == "3":
             print("CORRECT!")
-
+    print("")
     print("""Question B. What is the first thing Apple released?:
 1. Powerbook G4
 2. IBook
@@ -48,9 +48,46 @@ if choice == "1":
     else:
         print("try again")
         var2 = input("Question B. What is your Answer?: ")
-        if var1 == "4":
+        if var2 == "4":
             print("CORRECT!")
+    print("")
+    var3 = input("Question C. What is your name?: ")
 
-    var3 = input("Question 3. What is your name?: ")
+    print("")
+    print("Hey " + var3 + " you did pretty good. Thank you for playing my game, you can run it again to play a different level, also stay tuned for upcoming levels!")
 
-print("Hey " + var3 + " you did pretty good. Thank you for playing my game, stay tuned for upcoming levels!")
+if choice == "2":
+    print("""Question A. What is a synonym for situation?
+1. location
+2. position
+3. absence
+4. intermission""")
+    print("Type the number of your choice!")
+    var4 = input("Your Answer: ")
+    if var4 == "2":
+        print("CORRECT!")
+    else:
+        print("try again")
+        var4 = input("Question A. What is your Answer?: ")
+        if var4 == "2":
+            print("CORRECT!")
+    print("")
+    print("""Question B. Fill in the blank, The old man needed ____ to cross the street.
+1. assist
+2. time
+3. the cars to stop
+4. speed""")
+    print("Type the number of your choice!")
+    var5 = input("Your answer: ")
+    if var5 == "1":
+        print("CORRECT!")
+    else:
+        print("try again")
+        var5 = input("Question B. What is your Answer?: ")
+        if var5 == "1":
+            print("CORRECT!")
+    print("")
+    var3 = input("Question C. What is your name?: ")
+
+    print("")
+    print("Good Job " + var3 + "! Thank you for playing my game, you can run it again to play a different level, also stay tuned for upcoming levels!")
